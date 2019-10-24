cdf67322999612ee06b5caae130535846d95b364
Jennie-Watson <watson.jennie17@gmail.com>
2019-10-24 17:54:08 +1300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo2 from './logo2.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        <img src={logo2} className="stuff-logo" alt="logo" />
+        <h1>Lightning Talks</h1>
+          <p>
+            Lightning Talks are brief, 5-10 minute presentations that focus on a
+            single topic, example, idea, project, or technique. Lightning Talks
+            do not attempt to cover all aspects of their subject matter, but
+            present one facet of the idea clearly and succinctly.
+          </p>
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+         <button type="submit">Share your idea here</button>
       </header>
     </div>
   );
