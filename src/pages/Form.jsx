b10c16ae57f63ba8032f81c326605493b3d1ab70
Jennie-Watson <watson.jennie17@gmail.com>
2019-10-26 13:03:56 +1300

    ERROR - form textfield breaking when text typed

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -1,159 +1,104 @@
 import React from "react"
+import Form from "./Form"
 import TextField from "@material-ui/core/TextField"
 import Button from "@material-ui/core/Button"
+import "../styles/App.css"
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider"
 
-// import { createMuiTheme } from "@material-ui/core/styles"
-// import styles from "../styles/App.css"
-
-// const theme = createMuiTheme()
-
-// class Form extends React.Component {
-//   state = {
-//     email: "",
-//     topic: "",
-//     description: "",
-//   }
-
-// change = e => {
-//   this.props.onChange({ [e.target.name]: e.target.value })
-//   this.setState({
-//     [e.target.name]: e.target.value
-//   })
-// }
-
-// onSubmit = e => {
-//   e.preventDefault()
-//   // this.props.onSubmit(this.state)
-//   this.setState({
-//     email: "",
-//     topic: "",
-//     description: "",
-//   })
-//   this.props.onChange({
-//     email: "",
-//     topic: "",
-//     description: "",
-//   })
-// }
-
-// render() {
-//   return (
-//     <>
-//     <div className="form">
-//       <h1>Lightning Talks</h1>
-//     <form>
-//       <TextField
-//         id="outlined-multiline-static"
-//         label="Email"
-//         className="textField"
-//         margin="normal"
-//         variant="outlined"
-//         value={this.state.email} 
-//         onChange={e => this.change(e)}
-//       />
-//       <br />
-//       <TextField
-//         id="outlined-multiline-static"
-//         label="Topic"
-//         className="textField"
-//         margin="normal"
-//         variant="outlined"
-//         value={this.state.topic} 
-//         onChange={e => this.change(e)}
-//       />
-//       <br />
-//       <TextField
-//         id="outlined-multiline-static"
-//         label="Description"
-//         multiline
-//         rows="8"
-//         className="textField"
-//         margin="normal"
-//         variant="outlined"
-//         value={this.state.description} 
-//         onChange={e => this.change(e)}
-//         maxLength="120" // max word length is 120
-//       />
-//       <br />
-//       </form>
-//       </div>
-//       </>
-//     )
-//   }
-// }
-
-class Form extends React.Component {
+class Forms extends React.Component {
   state = {
-    email: '',
-    topic: '',
-    description: '',
+    email: "",
+    topic: "",
+    description: "",
+    fields: {}
   }
-change = e => {
-  this.props.onChange({ [e.target.name]: e.target.value })
-  this.setState({
-    [e.target.name]: e.target.value
-  })
-}
+
+  onChange = updatedValue => {
+    this.setState({
+      fields: {
+        ...this.state.fields,
+        ...updatedValue
+      }
+    })
+  }
+
+  // FORM BREAKING AND NOT RENDERING HERE
+  // <Form onChange={fields => this.onChange(fields)} /> 
+
+  change = e => {
+    this.props.onChange({ [e.target.name]: e.target.value })
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
 onSubmit = e => {
   e.preventDefault()
   // this.props.onSubmit(this.state)
   this.setState({
-    email: '',
-    topic: '',
-    description: '',
+    email: "",
+    topic: "",
+    description: "",
   })
+
   this.props.onChange({
-    email: '',
-    topic: '',
-    description: '',
+    email: "",
+    topic: "",
+    description: "",
   })
 }
 
 render() {
   return (
-    <form className="form">
-      {/* <h1 Lightning Talks /> // not working */}
-      <br />
-      <TextField
-        name="email"
-        label="Email"
-        id="outlined-multiline-static"
-        className="textField"
-        margin="normal"
-        variant="outlined"
-        value={this.state.email} 
-        onChange={e => this.change(e)}
-      />
-      <br />
-      <TextField
-        name="topic"
-        label="Topic"
-        id="outlined-multiline-static"
-        className="textField"
-        margin="normal"
-        variant="outlined"
-        value={this.state.topic} 
-        onChange={e => this.change(e)}
-      />
-      <br />
-      <TextField
-        name="description"
-        label="Description"
-        id="outlined-multiline-static"
-        multiline
-        rows="8"
-        className="textField"
-        margin="normal"
-        variant="outlined"
-        value={this.state.description} 
-        onChange={e => this.change(e)}
-        maxLength="120" // max word length is 120
-      />
-      <br />
-      <Button variant="contained" color="primary" label="Submit" onClick={e => this.onSubmit(e)} primary="true">Submit</Button>
-      </form>
+    <MuiThemeProvider>
+      <form className="form">
+        <TextField
+          name="email"
+          label="Email"
+          id="outlined-multiline-static"
+          className="textField"
+          margin="normal"
+          variant="outlined"
+          value={this.state.email} 
+          onChange={e => this.change(e)}
+          style={{width: "50%"}}
+        />
+        <br />
+        <TextField
+          name="topic"
+          label="Topic"
+          id="outlined-multiline-static"
+          className="textField"
+          margin="normal"
+          variant="outlined"
+          value={this.state.topic} 
+          onChange={e => this.change(e)}
+          style={{width: "50%"}}
+        />
+        <br />
+        <TextField
+          name="description"
+          label="Description"
+          id="outlined-multiline-static"
+          multiline
+          rows="8"
+          className="textField"
+          margin="normal"
+          variant="outlined"
+          value={this.state.description} 
+          onChange={e => this.change(e)}
+          maxLength="120" // max word length is 120
+          style={{width: "50%"}}
+        />
+        <br />
+        <Button variant="contained" color="primary" style={{width: "25%"}} onClick={e => this.onSubmit(e)} primary="true">Submit</Button>
+        <p>
+          {JSON.stringify(this.state.fields, null, 2)}
+        </p>
+        </form>
+      </MuiThemeProvider>
     )
   }
 }
 
-export default Form
+export default Forms
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,21 +1,16 @@
-// import React from "react"
-import React, { Component } from "react";
-// import { Link } from "react-router-dom"
-import Form from "./Form"
+import React, { Component } from "react"
+import { Link } from "react-router-dom"
+import logo from "../images/logo.png"
+// import Form from "./Form"
 
 // Styles
-import { createMuiTheme } from "@material-ui/core/styles"
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider"
-import styles from "../styles/App.css"
-
-import logo from "../images/logo.png"
-
-const theme = createMuiTheme()
+import "../styles/App.css"
 
 class MainPage extends Component {
   state = {
     fields: {}
-  };
+  }
 
   onChange = updatedValue => {
     this.setState({
@@ -23,65 +18,30 @@ class MainPage extends Component {
         ...this.state.fields,
         ...updatedValue
       }
-    });
-  };
+    })
+  }
 
   render() {
     return (
       <MuiThemeProvider>
         <div className="App">
-          <Form onChange={fields => this.onChange(fields)} />
+        <header className="App-header">
+          <img src={logo} className="logo" alt="logo" />
+          <h1>Lightning Talks</h1>
+          <p>
+          Lightning Talks are brief, 5-10 minute presentations that focus on a single topic, example, idea, project, or technique. Lightning Talks do not attempt to cover all aspects of their subject matter, but present one facet of the idea clearly and succinctly.
+          </p>
+          <Link to="/login" className="Login-link">Login Here</Link>
+          <Link to="/form" className="Form-link">Share your idea here</Link>
+          </header>
+          {/* <Form onChange={fields => this.onChange(fields)} />
           <p>
             {JSON.stringify(this.state.fields, null, 2)}
-          </p>
+          </p> */}
         </div>
       </MuiThemeProvider>
-    );
+    )
   }
 }
 
-export default MainPage;
-
-// class MainPage extends React.Component {
-//   state = {
-//     fields: {}
-//   }
-
-//   onChange = updatedValue => {
-//     this.setState({ 
-//       fields: {
-//         ...this.state.fields,
-//         ...updatedValue
-//       }
-//     })
-//   }
-  
-//   render() {
-//     return (
-//       <MuiThemeProvider theme={theme}>
-//         <div className="App">
-//           <header className="App-header">
-//             <img src={logo} className="logo" alt="logo" />
-//             <h1>Lightning Talks</h1>
-//               <p>
-//                 Lightning Talks are brief, 5-10 minute presentations that focus on a
-//                 single topic, example, idea, project, or technique. Lightning Talks
-//                 do not attempt to cover all aspects of their subject matter, but
-//                 present one facet of the idea clearly and succinctly.
-//               </p>
-//               <Link to="/login" className="Login-link">Login Here</Link>
-//               <Link to="/form" className="Form-link">Share your idea here</Link>
-//           </header>
-//           <div className="Form">
-//               <Form onChange={fields => this.onChange(fields)} />
-//                 <p>
-//                   {JSON.stringify(this.state.fields, null, 2)}
-//                 </p>
-//               </div>
-//         </div>
-//         </MuiThemeProvider>
-//       )
-//     }
-//   }
-  
-// export default MainPage
+export default MainPage
