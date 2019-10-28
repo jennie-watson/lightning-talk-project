9b8aab4aeaa7444509afc0ee36f5016c6e27b73d
Jennie-Watson <watson.jennie17@gmail.com>
2019-10-29 08:18:45 +1300

    Revert "google signin commented out and lightning bolt emoji added"
    This reverts commit 38522ecff2870c43966406c31678845765cd5477.

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -14,77 +14,78 @@ import Table from "./Table"
 import NotFoundPage from "./NotFoundPage"
 
 // Google Sign In
-// function NotSignedIn() {
-//   return (
-//     <div>
-//       <p>You are not signed in. Click here to sign in.</p>
-//       <button id="loginButton">Login with Google</button>
-//     </div>
-//   )
-// }
+function NotSignedIn() {
+  return (
+    <div>
+      <p>You are not signed in. Click here to sign in.</p>
+      <button id="loginButton">Login with Google</button>
+    </div>
+  )
+}
 
-// function Members() {
-//   return (
-//     <p>hello user, you're signed in </p>
-//   )
-// }
+function Members() {
+  return (
+    <p>hello user, you're signed in </p>
+  )
+}
 
 class App extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       isSignedIn: false,
-//     }
-//   }
-//   componentDidMount() {
-//     // window.gapi.load('auth2', () => {
-//     //   this.auth2 = gapi.auth2.init({
-//     //     client_id: '260896681708-o8bddcaipuisksuvb5u805vokq0fg2hc.apps.googleusercontent.com',
-//       // })
-//       this.auth2.attachClickHandler(document.querySelector('#loginButton'), {}, this.onLoginSuccessful.bind(this))
-//       this.auth2.then(() => {
-//         console.log('on init');
-//         this.setState({
-//           isSignedIn: this.auth2.isSignedIn.get(),
-//         });
-//       });
-//     // });    
-//   }
-//   onLoginSuccessful() {
-//     this.setState({
-//       isSignedIn: true,
-//       err: null
-//     })
-//   }
-//   onLoginFailed(err) {
-//     this.setState({
-//       isSignedIn: false,
-//       error: err,
-//     })
-//   }
-//   getContent() {
-//     if (this.state.isSignedIn) {
-//       return Members
-//     } else {
-//       return NotSignedIn
-//     }
+  // Google Sign In
+  constructor(props) {
+    super(props);
+    this.state = {
+      isSignedIn: false,
+    }
+  }
+  componentDidMount() {
+    window.gapi.load('auth2', () => {
+      this.auth2 = gapi.auth2.init({
+        client_id: '260896681708-o8bddcaipuisksuvb5u805vokq0fg2hc.apps.googleusercontent.com',
+      })
+      this.auth2.attachClickHandler(document.querySelector('#loginButton'), {}, this.onLoginSuccessful.bind(this))
+      this.auth2.then(() => {
+        console.log('on init');
+        this.setState({
+          isSignedIn: this.auth2.isSignedIn.get(),
+        });
+      });
+    });    
+  }
+  onLoginSuccessful() {
+    this.setState({
+      isSignedIn: true,
+      err: null
+    })
+  }
+  onLoginFailed(err) {
+    this.setState({
+      isSignedIn: false,
+      error: err,
+    })
+  }
+  getContent() {
+    if (this.state.isSignedIn) {
+      return Members
+    } else {
+      return NotSignedIn
+    }
     
-//   }
-//   requiresLogin(props, Component) {
-//     const promise = new Promise((resolve, reject) => {
-//       // window.gapi.load('auth2', () => {
-//       //   this.auth2 = gapi.auth2.init({
-//       //     client_id: '260896681708-o8bddcaipuisksuvb5u805vokq0fg2hc.apps.googleusercontent.com',
-//         })
+  }
+  requiresLogin(props, Component) {
+    const promise = new Promise((resolve, reject) => {
+      window.gapi.load('auth2', () => {
+        this.auth2 = gapi.auth2.init({
+          client_id: '260896681708-o8bddcaipuisksuvb5u805vokq0fg2hc.apps.googleusercontent.com',
+        })
   
-//         this.auth2.then(() => {
-//           return resolve(this.auth2.isSignedIn.get() ? <Component {...props}/> : <div>Need to login</div>)
-//         })
-//       // })
-//   //   }).then(component => {
-//   //     return component
-//   //   })    
-//   // }
+        this.auth2.then(() => {
+          return resolve(this.auth2.isSignedIn.get() ? <Component {...props}/> : <div>Need to login</div>)
+        })
+      })
+    }).then(component => {
+      return component
+    })    
+  }
 
   render() {
     return (
@@ -94,8 +95,8 @@ class App extends Component {
       <Route path="/form" component={Form} />
       <Route path="/login" component={Login} />
       <Route path="/table" component={Table} />
-      {/* <Route path="/members" render={this.requiresLogin.bind(this, Members)}/>
-      <Route path="/" component={NotSignedIn}/> */}
+      <Route path="/members" render={this.requiresLogin.bind(this, Members)}/>
+      <Route path="/" component={NotSignedIn}/>
       <Route exact path="/404" component={NotFoundPage} />
       <Redirect to="/404" />
       </Switch>
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,40 +1,40 @@
-// import React, {Component} from 'react';
-// // import Login from "./Login"
+import React, {Component} from 'react';
+// import Login from "./Login"
 
-// class Login extends Component {
-//   constructor(){
-//     super();
-//       this.state = {
-//         username: '',
-//         password: '',
-//         redirectToReferrer: false
-//         };
-//       this.login = this.login.bind(this);
-//       this.onChange = this.onChange.bind(this);
-//       }
-//       login() {
-//       //API Action Here
-//       }
+class Login extends Component {
+  constructor(){
+    super();
+      this.state = {
+        username: '',
+        password: '',
+        redirectToReferrer: false
+        };
+      this.login = this.login.bind(this);
+      this.onChange = this.onChange.bind(this);
+      }
+      login() {
+      //API Action Here
+      }
 
-//       onChange(e){
-//       this.setState({[e.target.name]:e.target.value});
-//       }
+      onChange(e){
+      this.setState({[e.target.name]:e.target.value});
+      }
 
-//       render() {
-//         return (
-//           <div className="row" id="Body">
-//           <div className="medium-5 columns left">
-//           <h4>Login</h4>
-//           <label>Username</label>
-//           <input type="text" name="username" onChange={this.onChange}/>
-//           <label>Password</label>
-//           <input type="password" name="password" onChange={this.onChange}/>
-//           <input type="submit" value="Login" onClick={this.login}/>
-//           <a href="/signup">Registration</a>
-//           </div>
-//           </div>
-//       );
-//     }
-//   }
+      render() {
+        return (
+          <div className="row" id="Body">
+          <div className="medium-5 columns left">
+          <h4>Login</h4>
+          <label>Username</label>
+          <input type="text" name="username" onChange={this.onChange}/>
+          <label>Password</label>
+          <input type="password" name="password" onChange={this.onChange}/>
+          <input type="submit" value="Login" onClick={this.login}/>
+          <a href="/signup">Registration</a>
+          </div>
+          </div>
+      );
+    }
+  }
 
-// export default Login
+export default Login
